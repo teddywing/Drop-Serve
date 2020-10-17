9580a6c5393fc88e38588032fc3310731d5fc36d
Teddy Wing <git@teddywing.com>
2020-10-17 16:23:11 +0200

    Add license (GNU GPLv3+)

--- Drop Serve/Server.m
+++ Drop Serve/Server.m
@@ -3,8 +3,22 @@
 //  Drop Serve
 //
 //  Created by TW on 10/10/20.
-//  Copyright (c) 2020 TW. All rights reserved.
+//  Copyright (c) 2020  Teddy Wing
 //
+//  This file is part of Drop Serve.
+//
+//  Drop Serve is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Drop Serve is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Drop Serve. If not, see <https://www.gnu.org/licenses/>.
 
 #import "Server.h"
 
